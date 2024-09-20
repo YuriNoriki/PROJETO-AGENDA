2c50b82d475dcab6573f30ad16bf53d022b1fc73
yuri noriki <yurinoriki@hotmail.com>
2024-09-20 17:58:39 -0300

    configurando models,admin de Contact e criando imagefield e configuranfo MEDIA_URL E MEDIA_ROOT

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    # Mostrando os campos no admin do django
+    list_display = 'id','first_name', 'last_name', 'phone',
+    # Para ficar na decrescente 
+    ordering = '-id', 
+    # list_filter = 'create_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    # Exibir uma valor por pagina 
+    list_per_page = 10
+    # define o limite máximo de itens que podem ser exibidos ao clicar em "Mostrar todos" no Django Admin.
+    list_max_show_all = 200
+    # Campos que podem ser editaveis
+    list_editable = 'first_name', 'last_name',
+
+    # Fica como link para acessar
+    list_display_links = 'id', 'phone',
--- contact/models.py
+++ contact/models.py
@@ -19,3 +19,14 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     create_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    
+    #No Django, quando você define um campo ImageField ou FileField com o parâmetro upload_to, 
+    # o caminho que você define será relativo ao diretório configurado em MEDIA_ROOT no seu arquivo de configurações (settings.py).
+    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
+    
+    '''esse método é especialmente útil quando você quer que os objetos do seu modelo sejam apresentados 
+    de maneira legível, tanto no terminal quanto na interface administrativa.'''
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
